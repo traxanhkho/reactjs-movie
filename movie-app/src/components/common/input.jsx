14075ca15173d336b243ac6224f87538926880ce
dat dang <38239103+traxanhkho@users.noreply.github.com>
2022-03-29 19:39:59 +0700

    Extracting a Reusable Form

--- movie-app/src/components/common/input.jsx
+++ movie-app/src/components/common/input.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const Input = ({ name, label, value, error, onChange }) => {
+const Input = ({ name, label,  error , ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
         id={name}
         name={name}
-        value={value}
-        onChange={onChange}
-        type="text"
+        {...rest}
         className="form-control"
       />
       {error && <div className="alert alert-danger">{error}</div>}
@@ -18,3 +16,4 @@ const Input = ({ name, label, value, error, onChange }) => {
 };
 
 export default Input;
+ // {...rest} not include name={name} ^^ .
--- movie-app/src/components/loginFrom.jsx
+++ movie-app/src/components/loginFrom.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Input from "./common/input";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
     data: {
       username: "",
@@ -10,67 +11,24 @@ class LoginForm extends Component {
     errors: {},
   };
 
-  validate = () => {
-    const errors = {};
-    const { data } = this.state;
-    if (data.username.trim() === "") errors.username = "Username is required.";
-    if (data.password.trim() === "") errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-    //call the server .
-    console.log("submitted!");
-  };
-
-  validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-    }
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const data = { ...this.state.data };
-    data[input.name] = input.value;
-    this.setState({ data, errors });
+  doSubmit = () => {
+    //Call the server .
+    console.log("Submitted.");
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            error={errors.username}
-            label="Username"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            error={errors.password}
-            label="Password"
-            onChange={this.handleChange}
-          />
-          <button className="btn btn-primary">Login</button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
